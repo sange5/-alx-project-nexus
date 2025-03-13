a5a4435d89c5e2f96f4e8d6c1343529b06769b7d
sange5 <163669819+sange5@users.noreply.github.com>
2025-03-13 17:39:48 +0300

    Optimized vote counting queries for performance

--- nexus/poll_system/polls/serializers.py
+++ nexus/poll_system/polls/serializers.py
@@ -4,16 +4,34 @@ from .models import Poll, Option, Vote
 class OptionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Option
-        fields = '__all__'
+        fields = ['id', 'text', 'poll']
 
 class PollSerializer(serializers.ModelSerializer):
-    options = OptionSerializer(many=True, read_only=True)
+    options = OptionSerializer(many=True, required=False)  
 
     class Meta:
         model = Poll
-        fields = '__all__'
+        fields = ['id', 'title', 'created_at', 'expiry_date', 'options']
+
+    def create(self, validated_data):
+        options_data = validated_data.pop('options', [])  
+        poll = Poll.objects.create(**validated_data)  
+
+        for option_data in options_data:
+            Option.objects.create(poll=poll, **option_data)  
+        return poll
 
 class VoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Vote
-        fields = '__all__'
+        fields = ['id', 'user', 'option']
+
+    def validate(self, data):
+        """Ensure a user can only vote once per poll."""
+        user = data.get('user')
+        option = data.get('option')
+
+        if Vote.objects.filter(user=user, option__poll=option.poll).exists():
+            raise serializers.ValidationError("You have already voted for this poll.")
+
+        return data
